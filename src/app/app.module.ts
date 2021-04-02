175b2a999d4c70b4682d9f44869e000ce1d22f01
Yakimovich Kanstantsin <yakimovichkostya@gmail.com>
2021-04-02 10:56:07 +0300

    feat: add basic structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
